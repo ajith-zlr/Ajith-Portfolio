ca9b1d4269dab3935fc4d7765643a50831649818
v.ajith <ajith@zelarsoft.com>
2023-08-18 13:58:55 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div style={{fontSize:42,fontWeight:"bold"}}>Hello HI Iam Ajith</div>
+    </div>
+  );
+}
+
+export default App;
