30bc0926c5a396e8aa656a2bb01ef3f99f2dee54
ajith-zlr <ajith@zelarsoft.com>
2024-09-06 18:01:30 +0530

    Auto-update code

---
+++ script.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div style={{fontSize:42,fontWeight:"semibold"}}>Hello HI Iam Ajith</div>
+    </div>
+  );
+}
+
+export default App;
